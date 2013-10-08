64cede850d40f553c9dbd313e9cf55cdecaa2b79
Max Korenkov <mkorenkov@gmail.com>
2013-10-08 21:47:28 +0300

    img dir

--- blueprints/dummy/__init__.py
+++ blueprints/dummy/__init__.py
@@ -3,12 +3,14 @@ from flask import Blueprint, render_template, redirect, url_for
 
 dummy = Blueprint("dummy", __name__, static_folder='static', static_url_path='/dummy/static', template_folder='templates')
 
+
 @dummy.route("/favicon.ico")
 def favicon():
-    return redirect(url_for('.static', filename='favicon.ico'))
+    return redirect(url_for('.static', filename='img/favicon.ico'))
+
 
 @dummy.route("/")
 def index():
     return render_template('index.haml',
                            index_css_url=url_for('.static', filename='index.css'),
-                           logo_url=url_for('.static', filename='logo.png'))
+                           logo_url=url_for('.static', filename='img/logo.png'))
