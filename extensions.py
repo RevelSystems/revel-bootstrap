2d4224d0698bd81f93b05cfdd5d16fe8d56f4ec8
Max Korenkov <mkorenkov@gmail.com>
2013-10-08 23:10:21 +0300

    mincss / minjs

--- extensions.py
+++ extensions.py
@@ -4,11 +4,12 @@ from jinja2_hamlpy import HamlPyExtension
 from flask.ext.assets import Environment, Bundle
 
 
-def find_sass():
+def find(pattern):
     for root, dir_names, file_names in os.walk('./'):
-        for filename in fnmatch.filter(file_names, '*.scss'):
-            scss_file = os.path.abspath(os.path.join(root, filename))
-            yield scss_file
+        for filename in fnmatch.filter(file_names, pattern):
+            matched_file = os.path.join(root, filename)
+            print "bundle {!r}".format(matched_file)
+            yield os.path.abspath(matched_file)
 
 
 def init_extensions(application):
@@ -17,9 +18,11 @@ def init_extensions(application):
 
     assets = Environment(application)
     assets.url = application.static_url_path
-    scss = Bundle(*list(find_sass()), filters='pyscss', output='all.css')
+    bootstrap_css = Bundle(*list(find('bootstrap*.css')), filters='cssmin', output='bootstrap.css')
+    scss = Bundle(*list(find('*.scss')), filters='pyscss', output='all.css')
+    assets.register('all.css', bootstrap_css, scss)
 
-    bundle = assets.register('all.css', scss)
-    print bundle
+    bootstrap_js = Bundle(*list(find('bootstrap*.js')), filters='jsmin', output='bootstrap.js')
+    assets.register('all.js', bootstrap_js)
     #assets.add(bundle)
     #bundle.build()
