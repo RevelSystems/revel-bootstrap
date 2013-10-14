6ded3ea94584212317fca1d13057d25e3f7e3dc0
Max Korenkov <mkorenkov@gmail.com>
2013-10-15 00:32:46 +0300

    session model

--- blueprints/dummy/models/__init__.py
+++ blueprints/dummy/models/__init__.py
@@ -1,12 +1,17 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+import uuid
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
+class SessionExpiredException(Exception):
+    pass
+
+
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(60))
     is_admin = Column(Boolean)
@@ -18,10 +23,35 @@ class User(Base):
         self.created = datetime.utcnow()
 
 
-class Tasks(Base):
-    __tablename__ = 'tasks'
+class Session(Base):
+    __tablename__ = "sessions"
     id = Column(Integer, primary_key=True)
-    issuer_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey("user.id"))
+    value = Column(String(16))
+    created = Column(DateTime)
+    expires = Column(DateTime)
+
+    def __init__(self, user):
+        self.user_id = user
+        self.value = unicode(uuid.uuid4())
+        self.created = datetime.utcnow()
+        self.expires = self.created + timedelta(hours=1)
+
+    def prolong(self):
+        if self.is_valid:
+            self.expires = datetime.utcnow() + timedelta(hours=1)
+        else:
+            raise SessionExpiredException("user_id {} session #{} has expired".format(self.user_id, self.id))
+
+    @property
+    def is_valid(self):
+        return datetime.utcnow() < self.expires
+
+
+class Tasks(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True)
+    issuer_id = Column(Integer, ForeignKey("user.id"))
     type = Column(String(20))
     status = Column(String(12))
     log = Column(Text)
